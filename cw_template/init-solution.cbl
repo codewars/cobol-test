ef5ce29761183ba7957496a345b58f890e8acbf5
Alexey Solovyev <solovyev.alexey@gmail.com>
2021-11-07 18:39:28 -0800

    misc

--- cw_template/init-solution.cbl
+++ cw_template/init-solution.cbl
@@ -8,7 +8,6 @@
        01  arg2        pic s9(5).
        01  result      pic s9(6).
 
-      * arg and result should be null terminated strings
        procedure division using arg1 arg2 result.
            compute result = 0
            goback.
