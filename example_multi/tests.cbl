232c3f7a2e2fd7036bf0090368ded15e358bbc69
Alexey Solovyev <solovyev.alexey@gmail.com>
2021-11-09 20:44:42 -0800

    example_multi: dynamic calls

--- example_multi/tests.cbl
+++ example_multi/tests.cbl
@@ -6,6 +6,7 @@
        01  i           pic 9(8).
        01  n           pic 9(10).
        01  n-disp      pic z(9)9.
+       01  func-name   pic x(30).
        01  result      pic 9(10)v9(2).
        01  expected    pic 9(10)v9(2).
 
@@ -28,6 +29,19 @@
                perform dotest-div
            end-perform
            
+           testsuite 'Dynamic Calls'.
+           perform varying i from 1 by 1 until i > 3
+               compute n = function random() * 20
+               if function random() > 0.5
+                   move 'mul2' to func-name
+                   compute expected = n * 2
+               else
+                   move 'div2' to func-name
+                   compute expected = n / 2
+               end-if
+               perform dotest-dynamic
+           end-perform
+
            end tests.
        
        dotest-div.
@@ -41,6 +55,15 @@
            move n to n-disp
            testcase "mul2: n = " function trim(n-disp).
            call 'mul2' using by content n by reference result
+           expect result to be expected
+           .
+
+       dotest-dynamic.
+           move n to n-disp
+           testcase function trim(func-name) 
+                    " (dynamic): n = " 
+                    function trim(n-disp).
+           call func-name using by content n by reference result
            expect result to be expected
            .
 
